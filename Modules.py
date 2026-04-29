99db1523802e44b1c2cffb883c95744db752d646
sandeepsatyavarapu2805 <sandeepsatyavarapu2805@gmail.com>
2026-04-29 22:13:36 +0530

    inital commit

--- Modules.py
+++ Modules.py
@@ -1,7 +1,8 @@
 import sys
-import random, math
+import random, math, datetime, calendar
 from my_modules import find_index as fi, test as t
 #import my_modules as mm then we can use mm.find_index()
+
 print(sys.path)
 
 courses = ['History','Math','Physics','Chemistry']
@@ -13,3 +14,9 @@ print('\n'+random_course)
 
 radian = math.radians(90)
 print(radian)
+print(math.sin(radian))
+
+today = datetime.date.today()
+print(today)
+
+print(calendar.isleap(today.year))
