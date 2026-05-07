203c1b3dcd0a319a04783033cb460b07be70367f
sandeepsatyavarapu2805 <sandeepsatyavarapu2805@gmail.com>
2026-05-07 13:27:22 +0530

    initial commit

--- parse_csv.py
+++ parse_csv.py
@@ -1,5 +1,6 @@
 import csv
 
+# using normal reader and writer of CSV
 with open('names.csv', 'r', newline='') as f, \
      open('names_new.csv', 'w', newline='') as nf:
 
@@ -15,3 +16,18 @@ with open('names.csv', 'r', newline='') as f, \
     # the above loop is good but we can also do this which is better
 
     writer.writerows(reader)
+
+
+# using dict reader and writer for CSV files
+with open('names.csv','r', newline='') as f, \
+     open('names_new.csv','w', newline='') as nf:
+    
+    reader_dict = csv.DictReader(f) # it formats the everything in the file into dictionary with keys as the file headers for every info
+
+    # for line in reader_dict:
+    #     print(line)
+    headers = reader_dict.fieldnames
+    writer_dict = csv.DictWriter(nf, fieldnames=headers, delimiter='-')
+    
+    writer_dict.writeheader()
+    writer_dict.writerows(reader_dict)
