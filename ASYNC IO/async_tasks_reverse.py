bb7729c41a64288b8911f3779a2fb0de8024cc9f
sandeepsatyavarapu2805 <sandeepsatyavarapu2805@gmail.com>
2026-06-08 23:02:19 +0530

    added sync, async files into ASYNCIO folder

--- ASYNC IO/async_tasks_reverse.py
+++ ASYNC IO/async_tasks_reverse.py
@@ -1,20 +1,23 @@
 import asyncio
 import time
 
+
 async def fetch_data(param):
     print(f"Do something with {param}...")
     await asyncio.sleep(param)
     print(f"Done with {param}")
     return f"Result of {param}"
 
+
 async def main():
-    task1 = fetch_data(1)  # Could be awaited directly
-    task2 = fetch_data(2)  # Could be awaited directly
-    result1 = await task1
-    print("Task 1 fully completed")
+    task1 = asyncio.create_task(fetch_data(1))
+    task2 = asyncio.create_task(fetch_data(2))
     result2 = await task2
     print("Task 2 fully completed")
+    result1 = await task1
+    print("Task 1 fully completed")
     return [result1, result2]
+
 
 t1 = time.perf_counter()
 
